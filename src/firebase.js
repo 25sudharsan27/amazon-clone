5dcc9a19fd7d86591957b7badf4718ce290c5801
Sudharsan B <25sudharsan27@gmail.com>
2023-07-28 19:12:07 +0530

    remove extra files

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD_ni55eja6Btmb_AnYj604GlaNN25V8NE",
+    authDomain: "clone-sudharsan.firebaseapp.com",
+    projectId: "clone-sudharsan",
+    storageBucket: "clone-sudharsan.appspot.com",
+    messagingSenderId: "1028606608695",
+    appId: "1:1028606608695:web:b5dc44be0dda53dace86b2",
+    measurementId: "G-GTFWWTWV18"
+  };
