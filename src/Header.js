fcd9f10040c8795bd73db8f34206c611330f6a7e
Sudharsan B <25sudharsan27@gmail.com>
2023-07-29 12:44:33 +0530

    complete navbar

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 import './Header.css';
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket"
 
 function Header(){
     return(
@@ -22,6 +23,10 @@ function Header(){
                 <div className="header__option">
                     <span className="header__optionLineOne">Your</span>
                     <span className="header__optionLineTwo">Prime</span>
+                </div>
+                <div className="header__optionBasket">
+                    <ShoppingBasketIcon/>
+                    <span className="header__optionLineTwo header__basketCount">0</span>
                 </div>
             </div>
         </div>
