173510abbb348b1902a324aaeab4aacfcbfb484e
Sudharsan B <25sudharsan27@gmail.com>
2023-07-30 11:42:08 +0530

    chekcout

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import './App.css';
 import React from "react";
 import Header from "./Header"
 import Home from "./Home"
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Checkout from "./Checkout"
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Home />
-    {/*Header */}
-    {/* Home */}
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+          </Switch>
+        
+        
+
     </div>
+    </Router>
 
   );
 }
---
+++ src/Checkout.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Checkout.css";
+
+function Checkout(){
+    return (
+        <div className="checkout">
+            <div className="checkout__left" height="100px" width=" 100px">
+                <img className="checkout__ad"
+                src="https://m.media-amazon.com/images/I/41FBwjCnS2L._SX1500_.jpg" width="100%" />
+            </div>
+        </div>
+    )
+}
+export default Checkout;
