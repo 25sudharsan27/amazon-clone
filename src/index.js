1551abaab94677be6e7f22ee526581e0f071257a
Sudharsan B <25sudharsan27@gmail.com>
2023-07-30 13:57:12 +0530

    state creation

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from "./serviceWorker"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <StateProvieder initialState={initialState} reducer={reducer}>
+      <App/>
+    </StateProvieder>
+    
+  </React.StrictMode>,
+  document.getElementById("root")
+
 );
 
 // If you want to start measuring performance in your app, pass a function
