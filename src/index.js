318023fea605612f41c6c18777c5e324d00cc50f
Sudharsan B <25sudharsan27@gmail.com>
2023-07-30 16:53:58 +0530

    reducer

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from "./serviceWorker"
-import {initialState} from "./reducer"
+import reducer, {initialState} from "./reducer"
+import {StateProvider} from "./StateProvider"
+
+
 
 ReactDOM.render(
   <React.StrictMode>
--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,18 @@
 export const initialState = {
     basket: [],
-};
+}; // This is the initial state of the basket
+
+const reducer =(state, action) => {
+    console.log(action);
+    switch(action.type) {
+        case 'ADD_TO_BASKET':
+            return {
+                ...state,
+                basket: [...state.basket,action.item],
+            };
+        default:
+            return state;
+    }
+}
+
+export default reducer;
