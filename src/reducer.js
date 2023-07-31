b8c8e75ed4f7b25dc3955e65195d1eb462697cac
Sudharsan B <25sudharsan27@gmail.com>
2023-07-31 18:41:23 +0530

    understanding

--- src/reducer.js
+++ src/reducer.js
@@ -3,15 +3,18 @@ export const initialState = {
 }; // This is the initial state of the basket
 
 const reducer =(state, action) => {
-    console.log(action);
+    console.log(action.type);
     switch(action.type) {
         case 'ADD_TO_BASKET':
             return {
                 ...state,
-                basket: [...state.basket,action.item],
+                basket: [...state.basket,action.item]
+                
             };
+            
         default:
             return state;
+            console.log("def")
     }
 }
 
