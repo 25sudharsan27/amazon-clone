423be083f007df6c95a6a81ce28b74ff053839ec
Sudharsan B <25sudharsan27@gmail.com>
2023-08-08 08:46:31 +0530

    modifications

--- src/Header.js
+++ src/Header.js
@@ -29,7 +29,7 @@ function Header(){
                 <Link to="/signin">
                 <div onClick={handleAuthentication} className="header__option">
                 
-                    <span id="sign" className="header__optionLineOne">{user.email}     </span><br/>
+                    <span id="sign" className="header__optionLineOne"> {user? "Hello "+user.email:"Hello Guest"} </span><br/>
                     
                     <span className="header__optionLineTwo"  >{user ? 'Sign Out':"Sign In"}</span>
                     
